0cc7416c18ab00622ef98e6811b5d505f0389e41
Neelesh-G <ne9884431281@gmail.com>
2024-05-08 17:02:53 -0400

    project_track and project display, email v2-fixed-server-error3

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,6 +1,10 @@
 import {getProviders, signIn} from "next-auth/react"
 
 export default function signin({providers}) {
+
+    if (!providers) {
+        return <p>No providers available or providers data failed to load.</p>;
+      }
   return (
     
         
