3bb900d45df44de80c432cb05615f68db94f96ea
Neelesh-G <ne9884431281@gmail.com>
2024-05-08 23:02:35 -0400

    project_track and project display, email v2-fixed-server-error10

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -2,12 +2,18 @@
 export default function signin({providers}) {
 
     return (
-        <div>
-            <h1>Testing Sign-In Page</h1>
-            <p>If you can see this, the sign-in page is loading correctly.</p>
-            <p>This is the secon paragraph</p>
-        </div>
-    );
+        <div className="flex justify-center mt-40">
+
+            {Object.values(providers).map((provider)=>(
+
+            <p className="text-center text-xl  my-10">Welcome to Yoso, your biggest workplace advocate</p>
+
+
+
+))}
+</div>
+        
+    )
 }
 
 {/*import {getProviders, signIn} from "next-auth/react"
