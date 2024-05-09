044af7d6d57776555fbfe5397f219a22fbb5f49c
Neelesh-G <ne9884431281@gmail.com>
2024-05-08 22:03:02 -0400

    project_track and project display, email v2-fixed-server-error6

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,7 +19,7 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -2,25 +2,20 @@ import {getProviders, signIn} from "next-auth/react"
 
 export default function signin({providers}) {
 
-    if (!providers) {
-        return <p>No providers available or providers data failed to load.  {providers}</p>;
-      }
-  return (
     
-        
-        
+  return (
+
 
         <div className="flex justify-center mt-40">
 
             {Object.values(providers).map((provider)=>(
-                <div key={provider.name} className="flex flex-col items-center">
+                <div key={providers.name} className="flex flex-col items-center">
                     <div className="flex items-center">
                         <img className="h-20 w-25 object-cover" src="https://thumbs.dreamstime.com/z/print-171223965.jpg" 
                         alt="Y part of Yoso"/>
                         <span className="text-5xl font-bold" style={{ color: '#87cdf5' }}>oso</span>
                     </div>
                     <p className="text-center text-xl  my-10">Welcome to Yoso, your biggest workplace advocate</p>
-                    <button onClick={()=>signIn(provider.id, {callbackUrl:"/"})} className="bg-sky-300 rounded-lg p-3 text-white hover:bg-sky-500">Sign in with {provider.name}</button>
                     <button onClick={()=>signIn(provider.id, {callbackUrl:"/"})} className="bg-sky-300 rounded-lg p-3 text-white hover:bg-sky-500">Sign in with {provider.name}</button>
 
                 </div>
@@ -32,7 +27,7 @@ export default function signin({providers}) {
 
 
 
-/*
+
 export async function getServerSideProps()
 {
 
@@ -43,32 +38,5 @@ export async function getServerSideProps()
             providers,
         },
     }
-}*/
-
-
-
-export async function getServerSideProps(context) {
-    let providers = null;
-    console.log("hello world")
-    try {
-        providers = await getProviders();
-        if (!providers) {
-            throw new Error('No authentication providers found.');
-        }
-    } catch (error) {
-        console.error('Failed to fetch providers:', error);
-        // Optionally, return an error message to display or handle it in the component
-        return {
-            props: {
-                error: 'Failed to fetch authentication providers.'
-            },
-        };
-    }
-
-    return {
-        props: {
-            providers,
-        },
-    };
 }
 
