67cc6a4a05d1147255acfbabd8a591acab62dc89
Neelesh-G <ne9884431281@gmail.com>
2024-05-09 03:09:40 -0400

    project_track and project display, email v2-fixed-server-error21

--- components/LandingComp/Feature.js
+++ components/LandingComp/Feature.js
@@ -14,7 +14,8 @@ export default function Feature() {
       </div>
       <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
         <h2 class="text-gray-900 text-lg title-font font-medium mb-2">Feature 1</h2>
-        <p class="leading-relaxed text-base">Feature 1 info</p>
+        <p class="leading-relaxed text-base">The first feature is a collaboration tool designed to facilitate engagement in ad hoc projects. This tool allows employees to contribute to smaller tasks outside their main responsibilities, fostering a collaborative culture within the company. It also maintains validated records of these contributions, recognized by both managers and peers. This not only encourages a more cohesive work environment but also ensures that no effort goes unnoticed.
+</p>
         <a class="mt-3 text-blue-500 inline-flex items-center">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -25,7 +26,8 @@ export default function Feature() {
     <div class="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
       <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
         <h2 class="text-gray-900 text-lg title-font font-medium mb-2">Feature 2</h2>
-        <p class="leading-relaxed text-base">Feature 2 info</p>
+        <p class="leading-relaxed text-base">The second standout feature of Yoso is its AI-powered reporting system. This advanced feature generates comprehensive reports on an employee's achievements, including all endorsements received and an analysis of their work ethic. This aspect of the app is particularly beneficial during performance reviews, offering a detailed and accurate representation of an employee's contributions and capabilities. By highlighting these achievements, Yoso paves the way for rewards, benefits, and promotions, thereby incentivizing productivity and enhancing job satisfaction.
+</p>
         <a class="mt-3 text-blue-500 inline-flex items-center">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -41,13 +43,13 @@ export default function Feature() {
       </div>
     </div>
     <div class="flex items-center lg:w-3/5 mx-auto sm:flex-row flex-col">
-      <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+     {/* <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
           <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
           <circle cx="12" cy="7" r="4"></circle>
-        </svg>
-      </div>
-      <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+  </svg>
+      </div>*/}
+     {/* <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
         <h2 class="text-gray-900 text-lg title-font font-medium mb-2">Feature 3</h2>
         <p class="leading-relaxed text-base">Feature 3 info</p>
         <a class="mt-3 text-blue-500 inline-flex items-center">Learn More
@@ -55,11 +57,11 @@ export default function Feature() {
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
         </a>
-      </div>
+      </div>*/}
     </div>
     <div className='flex justify-center'>
-    <button class="flex mx-auto mt-20 text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Add to Slack</button>
-    <button class="flex mx-auto mt-20 text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Add to Teams</button>
+    {/*<button class="flex mx-auto mt-20 text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Add to Slack</button>
+    <button class="flex mx-auto mt-20 text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Add to Teams</button>*/}
   </div>
   </div>
 </section>
--- components/LandingComp/Hero.js
+++ components/LandingComp/Hero.js
@@ -6,17 +6,17 @@ export default function Hero() {
         
         <section class="text-gray-600 body-font">
   <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
+     {/*<img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dumhttps://media.licdn.com/dms/image/D5612AQEXloeCtCVPEw/article-cover_image-shrink_720_1280/0/1684795796813?e=1720656000&v=beta&t=QmpRM4_EfY-iZwVzDM-A1w5gI4Cqss1jzrU_ZcbGH5Qyimage.com/720x600"/> */}
     <div class="text-center lg:w-2/3 w-full">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Welcome to Yoso, your biggest workplace advocate</h1>
       <p class="mb-8 leading-relaxed">Welcome to Yoso: Where Growth and Collaboration Thrive!
 
-At Yoso, we believe that every company deserves a platform that cultivates the true potential of its team members. Our mission is to empower organizations with a powerful tool that goes beyond just connecting colleagues - we foster genuine engagement, productivity, and a culture of growth.
+      Yoso is poised to revolutionize the workplace as the ultimate advocate for employees. It addresses a significant gap in the corporate world: the under-recognition of employees' extra efforts and the inefficiencies in managing ad hoc projects alongside main tasks. Yoso introduces a dual-feature solution to tackle these challenges.
 </p>
       <div class="flex justify-center">
         {/* bg-indigo-500 */}
-        <button class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+        <button class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Login with Teans</button>
+        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Login with Slack</button>
       </div>
     </div>
   </div>
